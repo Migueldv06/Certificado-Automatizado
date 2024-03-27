abf63e4bb220f5ddacf67f8b1dee6cc7d4286093
Miguel Domiciano Vieira <97977665+Migueldv06@users.noreply.github.com>
2024-03-27 16:04:03 -0300

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,9 +1,9 @@
 <?php 
-$emailhost = "smtp.ambientelivre.com.br";//coloque seu dominio de email
-$email = "miguel@ambientelivre.com.br";//coloque o email de saida
-$emailsenha = "#Migueldv1";//coloque a senha do seu email
+$emailhost = 'mail.seuDominio.com.br';//coloque seu dominio de email
+$email = 'conta@SeuDomínio.com.br';//coloque o email de saida
+$emailsenha = 'SuaSenha';//coloque a senha do seu email
 $arquivoCSV = "Nome.csv";//coloque o seu arquivo com as informações csv
-$SeuNome = "Ambiente Livre";//Seu Nome *opcional
+$SeuNome = "";//Seu Nome *opcional
 
 //info do treinamento Exemplo
 $data = "05/02/2024 a 06/03/2024";
